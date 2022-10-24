73e095c74a80d6354565ae5d4f68af0833fe122f
judithquine <judithquine@yahoo.es>
2022-10-25 00:12:14 +0200

    added component TestApi file

---
+++ src/components/TestApi.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    getTodos() {
+      try {
+        fetch("https://todos-mpwar.herokuapp.com/users/aleh/todos")
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            this.todos = data;
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getTodos();
+  },
+};
+</script>
+
+<template>
+  <div>
+    <header>
+      <h1>Todo</h1>
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">{{ todo.text }}</li>
+      </ul>
+    </header>
+  </div>
+</template>
